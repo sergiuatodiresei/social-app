718ba13f9ab65702610764d3334efcf789b463e4
Sergiu Atodiresei <sergiu_atodiresei@MacBook-Pro-Sergiu.local>
2016-11-30 22:24:38 +0200

    first commit

---
+++ Social Network/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social Network
+//
+//  Created by Sergiu Atodiresei on 30.11.2016.
+//  Copyright © 2016 SergiuApps. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
