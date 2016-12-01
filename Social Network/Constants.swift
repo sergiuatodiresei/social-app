ca8e57d2618a6acb72863ed08680221d4c137a6c
Sergiu Atodiresei <sergiu_atodiresei@MacBook-Pro-Sergiu.local>
2016-12-01 02:35:50 +0200

    Sign in UI custom classes complete

---
+++ Social Network/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Social Network
+//
+//  Created by Sergiu Atodiresei on 01.12.2016.
+//  Copyright © 2016 SergiuApps. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
