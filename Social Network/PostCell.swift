6d0185442e21dbbde30b5ce212b5929f3c5dff05
Sergiu Atodiresei <sergiu_atodiresei@MacBook-Pro-Sergiu.local>
2017-01-06 17:50:00 +0200

    Complete Firebase storage download

--- Social Network/PostCell.swift
+++ Social Network/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
     
@@ -15,14 +16,34 @@ class PostCell: UITableViewCell {
     @IBOutlet weak var postImg: UIImageView!
     @IBOutlet weak var caption: UITextView!
     @IBOutlet weak var likesLbl: UILabel!
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
     
-    func configureCell(post: Post) {
+    func configureCell(post: Post, img: UIImage? = nil) {
         self.caption.text = post.caption
         self.likesLbl.text = "\(post.likes)"
+        
+        if img != nil {
+            self.postImg.image = img
+        } else {
+            let ref = FIRStorage.storage().reference(forURL: post.imageUrl)
+            ref.data(withMaxSize: 2 * 1024 * 1024, completion: { (data, error) in
+                if error != nil {
+                    print("Sergiu: Unable to download from Firebase storage")
+                } else {
+                    print("Sergiu: Image downloaded from Firebase storage")
+                    if let imgData = data {
+                        if let img = UIImage(data: imgData) {
+                            self.postImg.image = img
+                            FeedVC.imageCache.setObject(img, forKey: post.imageUrl as NSString)
+                        }
+                        
+                    }
+                }
+            })
+        }
     }
 
 }
